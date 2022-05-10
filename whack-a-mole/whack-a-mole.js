7d795a86b57cc97b8493de4844e83c0b1abed537
Yuri Cowan <yuricowan11@gmail.com>
2022-05-10 13:00:23 +1200

    What-a-mole: random number generator created

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,26 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+console.log('Whack-a-Mole!')
+
+let cells = document.getElementsByTagName('TD')
+let mole = `<img src="mole.png">`
+
+// -- Random number generator -- //
+
+let max = cells.length
+let randomNumber = 0
+getRandomInt = (max) => (randomNumber = Math.floor(Math.random() * (max + 1)))
+
+for (i = 0; i < cells.length; i++) {
+  cells[i].onclick = cellClicked
+}
+
+function cellClicked(e) {
+  let cell = e.target
+
+  getRandomInt(max)
+  console.log(randomNumber)
+  cell.innerhtml = mole
+}
