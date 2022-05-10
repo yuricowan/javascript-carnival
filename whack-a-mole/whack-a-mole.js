277b3d94b8641ad7f95e55f58a1030b6854374fd
Yuri Cowan <yuricowan11@gmail.com>
2022-05-10 16:05:58 +1200

    Whack-a-mole complete, I got tripped up on my if statement which took a lifetime to resolve. Had to seek help

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -5,22 +5,41 @@
 console.log('Whack-a-Mole!')
 
 let cells = document.getElementsByTagName('TD')
-let mole = `<img src="mole.png">`
+let imageElement = document.createElement('img')
+let whackSound = new Audio(`noot-noot.mp3`)
+imageElement.src = 'mole.png'
+imageElement.className = 'moleSize'
 
 // -- Random number generator -- //
-
 let max = cells.length
 let randomNumber = 0
-getRandomInt = (max) => (randomNumber = Math.floor(Math.random() * (max + 1)))
+getRandomInt = (max) => (randomNumber = Math.floor(Math.random() * max))
+
+placeMole = () => {
+  cells[randomNumber].appendChild(imageElement)
+}
+
+removeMole = () => {
+  cells[randomNumber].removeChild(imageElement)
+}
 
 for (i = 0; i < cells.length; i++) {
   cells[i].onclick = cellClicked
 }
 
 function cellClicked(e) {
-  let cell = e.target
-
-  getRandomInt(max)
-  console.log(randomNumber)
-  cell.innerhtml = mole
+  if (cells[randomNumber].contains(e.target) == true) {
+    whackAMoleSound()
+    removeMole()
+    getRandomInt(max)
+    placeMole()
+  }
 }
+
+whackAMoleSound = () => {
+  whackSound.play()
+}
+
+// -- ON START -- //
+getRandomInt(max)
+placeMole()
